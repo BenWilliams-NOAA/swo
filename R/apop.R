02799daed7c11daac59d5264fd7e5877635a35d0
Ben <ben.williams@noaa.gov>
2022-07-26 13:29:43 -0800

    sizepop

--- R/apop.R
+++ R/apop.R
@@ -61,7 +61,7 @@ apop <- function(lpop, agedat, strata){
                            .by = c(year, species_code, stratum, age)) %>%
         tidytable::select.(year, species_code, stratum, sex, age, agepop) %>% 
         unique(., by = c('year', 'species_code', 'stratum', 'sex', 'age', 'agepop')) %>%
-        tidytable::select.(-c(length, age_num, age_frac, size_pop)) %>% 
+        tidytable::select.(-c(length, age_num, age_frac, sizepop)) %>% 
         tidytable::bind_rows.(.agepop_mf) %>%
         tidytable::pivot_wider.(names_from = sex, values_from = agepop, values_fill = 0) %>%
         tidytable::rename.(unsexed = '3', males = '1', females = '2') 
@@ -111,7 +111,7 @@ apop <- function(lpop, agedat, strata){
       tidytable::mutate.(agepop = sum(agepop), 
                          .by = c(year, species_code, age)) %>%
       unique(., by = c('year', 'species_code', 'sex', 'age', 'agepop')) %>%
-      tidytable::select.(-c(length, age_num, age_frac, size_pop)) %>% 
+      tidytable::select.(-c(length, age_num, age_frac, sizepop)) %>% 
       tidytable::bind_rows.(.agepop_mf) %>%
       tidytable::pivot_wider.(names_from = sex, values_from = agepop, values_fill = 0) %>%
       tidytable::rename.(unsexed = '3', males = '1', females = '2') 
