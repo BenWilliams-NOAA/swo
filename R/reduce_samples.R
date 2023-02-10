476cabb1265eda7bc56bf683ae3fd27c99ee70a5
pete-hulson <pete.hulson@noaa.gov>
2023-02-10 13:19:15 -0900

    too many changes for issues, gonna do pull request

--- R/reduce_samples.R
+++ R/reduce_samples.R
@@ -3,12 +3,28 @@
 #' @param data lfreq or afreq dataframe
 #' @param samples number of lengths to take
 #' @param grp grouping variable(s)
+#' @param type either age or length reduction (default = NULL)
 #'
 #' @return
 #' @export reduce_samples
 #'
 #'
 #' @examples
-reduce_samples <- function(data, samples, grp = c('year', 'species_code', 'stratum', 'hauljoin')){
-  core_samp(data, samples, grp = grp, replace = FALSE)
+reduce_samples <- function(data, samples, grp = c('year', 'species_code', 'stratum', 'hauljoin'), type = NULL){
+
+  data %>% 
+    tidytable::mutate.(id = .I) -> .inter
+  
+  if(type == 'length'){
+    core_samp(.inter, samples, grp = grp, replace = FALSE) -> .new_samp
+  } else if (type == 'age'){
+    .inter %>% 
+      tidytable::slice_sample(prop = samples, .by = c(year, species_code)) -> .new_samp
+  }
+
+  .inter %>%
+    tidytable::anti_join.(.new_samp, by = "id") -> .redux_samp
+  
+  .out = list(data = .new_samp, nosamp = .redux_samp)
+
 }
