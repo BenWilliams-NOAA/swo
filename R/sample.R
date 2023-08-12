f494aa0c5693c38f3ba3ebf1588b77279f64d1ae
Ben <ben.williams@noaa.gov>
2023-08-11 16:18:50 -0800

    remove .dots

--- R/sample.R
+++ R/sample.R
@@ -8,9 +8,9 @@ sample <- function(lfreq_un, samples, type = 'length') {
   
   if(type == 'length'){
     lfreq_un %>%
-      tidytable::filter.(sex != 3) %>%
-      tidytable::mutate.(id = .I) %>%
-      tidytable::mutate.(n = .N, 
+      tidytable::filter(sex != 3) %>%
+      tidytable::mutate(id = .I) %>%
+      tidytable::mutate(n = .N, 
                          .by = c(year, species_code, stratum, hauljoin)) -> .inter
     
     # sample by sample size
@@ -18,7 +18,7 @@ sample <- function(lfreq_un, samples, type = 'length') {
     
     .inter %>%
       tidytable::anti_join.(.new_sexed, by = "id") %>%
-      tidytable::mutate.(sex = 3) -> .new_unsexed
+      tidytable::mutate(sex = 3) -> .new_unsexed
     
     # if(isTRUE(write_sample)){
     #   .new_unsexed %>% 
@@ -29,10 +29,10 @@ sample <- function(lfreq_un, samples, type = 'length') {
     
     # rejoin to original unsexed
     lfreq_un %>%
-      tidytable::filter.(sex == 3) %>%
-      tidytable::mutate.(id = .I,
+      tidytable::filter(sex == 3) %>%
+      tidytable::mutate(id = .I,
                          n = .N, .by = c(year, species_code, stratum, hauljoin)) %>%
-      tidytable::bind_rows.(.new_sexed, .new_unsexed)  -> .data
+      tidytable::bind_rows(.new_sexed, .new_unsexed)  -> .data
     
 
       .new_unsexed %>% 
